0a46d2d44c514d935688ec6b569f864a23897f39
Francois <francois.thullier98@gmail.com>
2025-08-20 10:16:44 +0200

    Aspect ratio of real image and on screen image taken into account

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -1,54 +1,121 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "../components/ui/Modal";
-import waldoEasy from "../assets/2687205.png";
+import waldoEasy from "../assets/waldo_beach_large.jpg";
 import Countdown from "../components/Countdown";
 import { usePlayer } from "../context/playerContext";
 import { useCounter } from "../context/counterContext";
+import { useAllPlayers } from "../context/allPlayersContext";
+import convertTime from "../utils/timeFormat";
+import getMouseClickPosition from "../utils/getMouseCoordinates";
+import Select from "../components/ui/Select";
 function Game() {
-  const [message, setMessage] = useState<string>("You did not find Waldo");
+  type MousePos = {
+    xScreen: number;
+    yScreen: number;
+    xImage: number;
+    yImage: number;
+  };
+
+  type ImageProperties = {
+    top: number;
+    left: number;
+    naturalWidth: number;
+    naturalHeight: number;
+    onScreenWidth: number;
+    onScreenHeight: number;
+  };
+  const [mousePos, setMousePos] = useState<MousePos>({
+    xScreen: 0,
+    yScreen: 0,
+    xImage: 0,
+    yImage: 0,
+  });
+
+  const [message, setMessage] = useState<string>(
+    "Click on the image and find Waldo and his friends!"
+  );
   const [isOver, setIsOver] = useState<boolean>(false);
   const { player, setPlayer } = usePlayer();
-  const { counter } = useCounter();
+  const { counter, setCounter } = useCounter();
+  const { setAllPlayers } = useAllPlayers();
+  const { minutes, secs } = convertTime(counter); // get original
+  const [onScreenImageSize, setOnScreenImageSize] = useState<ImageProperties>({
+    top: 0,
+    left: 0,
+    naturalWidth: 0,
+    naturalHeight: 0,
+    onScreenWidth: 0,
+    onScreenHeight: 0,
+  });
 
-  const whoWasFound = (xPos: number, yPos: number) => {
-    // Waldo position
+  // const image dimensions
 
-    const waldoXmin: number = 490;
+  const [hasStarted, setHasStarted] = useState(false);
+  // original image dimensions
+  const [outsideImageClick, setOutsideImageClick] = useState(false);
+  // removes the dropdown is the user clicks away
 
-    const waldoXmax: number = 528;
+  const handleScreenClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const { x, y } = getMouseClickPosition(e);
+    const naturalWidth = onScreenImageSize.naturalWidth;
 
-    const waldoYmin: number = 258;
-
-    const waldoYmax: number = 311;
-
+    const aspectRatio = naturalWidth / onScreenImageSize.onScreenWidth;
+    const xRealImage = x * aspectRatio;
+    const yRealImage = y * aspectRatio;
+    setHasStarted(true);
+    setMousePos({
+      xScreen: x,
+      yScreen: y,
+      xImage: xRealImage,
+      yImage: yRealImage,
+    });
     if (
-      waldoXmin < xPos &&
-      waldoXmax > xPos &&
-      waldoYmin < yPos &&
-      waldoYmax > yPos
+      x < onScreenImageSize.left ||
+      x > onScreenImageSize.onScreenWidth + onScreenImageSize.left ||
+      y < onScreenImageSize.top ||
+      y > onScreenImageSize.onScreenHeight + onScreenImageSize.top
     ) {
-      setIsOver(true);
-      setPlayer({ name: player.name, score: counter });
-      setMessage(`You found Waldo in ${counter} secs!!`);
-
-      return true;
+      setOutsideImageClick(true);
     } else {
-      return false;
+      setOutsideImageClick(false);
+      setMousePos({
+        xScreen: x,
+        yScreen: y,
+        xImage: (x - onScreenImageSize.left) * aspectRatio,
+        yImage: (y - onScreenImageSize.top) * aspectRatio,
+      });
     }
   };
 
-  const getMouseClickPosition = (
-    e: React.MouseEvent<HTMLImageElement, MouseEvent>
-  ) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - Math.round(rect.top);
-    whoWasFound(x, y);
-    return { x, y };
+  const handleImageLoad = (e: React.SyntheticEvent<HTMLImageElement>) => {
+    const img = e.currentTarget;
+    const rect = img.getBoundingClientRect();
+    setOnScreenImageSize({
+      top: rect.top,
+      left: rect.left,
+      naturalWidth: img.naturalWidth,
+      naturalHeight: img.naturalHeight,
+      onScreenWidth: rect.width,
+      onScreenHeight: rect.height,
+    });
   };
 
+  useEffect(() => {
+    setCounter(0);
+    if (isOver) {
+      setPlayer({ name: player.name, score: `${minutes}:${secs}` });
+      setAllPlayers((prevArray) => {
+        return [...prevArray, { name: player.name, score: counter }];
+      });
+      // setMessage(`You found Waldo in ${minutes}min${secs}sec !!`);
+    }
+  }, [isOver]);
+
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div
+      className="hero bg-base-200 min-h-screen"
+      onClick={(e) => handleScreenClick(e)}
+    >
       <Modal />
       <div className="hero-content text-center flex-col">
         <div className="max-w-md">
@@ -56,11 +123,27 @@ function Game() {
           <Countdown isOver={isOver} />
           <p>{message}</p>
         </div>
-        <img
-          onClick={(e) => getMouseClickPosition(e)}
-          src={waldoEasy}
-          className=" cursor-pointer max-w-6xl rounded-lg shadow-2xl"
-        />
+        <div className="">
+          {!hasStarted || isOver || outsideImageClick ? null : (
+            <>
+              <Select
+                dropdownXPos={mousePos.xScreen}
+                dropdownYPos={mousePos.yScreen}
+                imageX={mousePos.xImage}
+                imageY={mousePos.yImage}
+                setIsOver={setIsOver}
+                setMessage={setMessage}
+              />
+            </>
+          )}
+
+          <img
+            // onClick={(e) => handleImageClicks(e)}
+            src={waldoEasy}
+            className=" cursor-pointer max-w-6xl rounded-lg shadow-2xl"
+            onLoad={(e) => handleImageLoad(e)}
+          />
+        </div>
       </div>
     </div>
   );
--- client/src/utils/TimeFormat.ts
+++
@@ -1,9 +0,0 @@
-const convertTime = (counter: number) => {
-  const hours = Math.floor(counter / 3600);
-  const minutes = Math.floor((counter % 3600) / 60);
-  const secs = counter % 60;
-
-  return { hours, minutes, secs };
-};
-
-export default convertTime;
