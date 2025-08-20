ee585eac743b9aeccb1d534ea6cc7d75155d6a97
Francois <francois.thullier98@gmail.com>
2025-08-20 10:15:12 +0200

    Grouped all contexts

--- client/src/context/Provider.tsx
+++ client/src/context/Provider.tsx
@@ -1,15 +1,17 @@
 import type { ReactNode } from "react";
 import { CounterProvider } from "./counterContext";
 import { PlayerProvider } from "./playerContext";
-
+import { AllPlayersProvider } from "./allPlayersContext";
 type Props = {
   children: ReactNode;
 };
 function Provider({ children }: Props) {
   return (
-    <CounterProvider>
-      <PlayerProvider>{children}</PlayerProvider>
-    </CounterProvider>
+    <AllPlayersProvider>
+      <CounterProvider>
+        <PlayerProvider>{children}</PlayerProvider>
+      </CounterProvider>
+    </AllPlayersProvider>
   );
 }
 
