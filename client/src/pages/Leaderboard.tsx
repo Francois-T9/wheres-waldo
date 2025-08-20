7edec1eddd89373ae3e21e9d873b1f38c25156c4
Francois <francois.thullier98@gmail.com>
2025-08-20 10:16:10 +0200

    Sorted array by score before displaying the rankings

--- client/src/pages/Leaderboard.tsx
+++ client/src/pages/Leaderboard.tsx
@@ -1,39 +1,34 @@
-import { usePlayer } from "../context/playerContext";
-
+import { useAllPlayers } from "../context/allPlayersContext";
+import sortArrayByScore from "../utils/sortArrayByScore";
 function Leaderboard() {
-  const { player } = usePlayer();
-  console.log(player);
+  const { allPlayers } = useAllPlayers();
+
+  const sortedPlayers = sortArrayByScore(allPlayers);
   return (
     <div className="hero bg-base-200 min-h-screen flex items-start justify-center p-4">
-      <ul className="list bg-base-100 rounded-box shadow-md w-[50%]">
-        <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">
-          Players ranking
-        </li>
-
-        <li className="list-row">
-          <div className="text-4xl font-thin opacity-30 tabular-nums">1</div>
-          <div></div>
-          <div className="list-col-grow">
-            <div>Dio Lupa</div>
-          </div>
-        </li>
-
-        <li className="list-row">
-          <div className="text-4xl font-thin opacity-30 tabular-nums">2</div>
-          <div></div>
-          <div className="list-col-grow">
-            <div>Ellie Beilish</div>
-          </div>
-        </li>
-
-        <li className="list-row">
-          <div className="text-4xl font-thin opacity-30 tabular-nums">3</div>
-          <div></div>
-          <div className="list-col-grow">
-            <div>Sabrino Gardener</div>
-          </div>
-        </li>
-      </ul>
+      <div className="overflow-x-auto">
+        <table className="table table-xl ">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          {sortedPlayers.map((player, key) => (
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>{key + 1}</th>
+                <td>{player.name}</td>
+                <td>{player.score}</td>
+              </tr>
+              {/* row 2 */}
+            </tbody>
+          ))}
+        </table>
+      </div>
     </div>
   );
 }
