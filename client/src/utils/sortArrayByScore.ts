6e112016ff2134b68742158d2b134bbca2162644
Francois <francois.thullier98@gmail.com>
2025-08-25 12:09:19 +0200

    Sorted array by score and omitting 0 score (default)

--- client/src/utils/sortArrayByScore.ts
+++ client/src/utils/sortArrayByScore.ts
@@ -5,7 +5,9 @@ interface Player {
 const sortArrayByScore = (array: Array<Player>) => {
   const sortedArray = array.sort((a, b) => a.score - b.score);
 
-  return sortedArray;
+  const nonNullSortedArray = sortedArray.filter((player) => player.score !== 0);
+
+  return nonNullSortedArray;
 };
 
 export default sortArrayByScore;
