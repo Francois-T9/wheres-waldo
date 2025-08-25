65714b02f9de05e422493dd8aaa962cef1659cfa
Francois <francois.thullier98@gmail.com>
2025-08-25 12:10:19 +0200

    Removed hardcode

--- client/src/utils/whoWasFound.ts
+++ client/src/utils/whoWasFound.ts
@@ -1,28 +1,24 @@
-// function that inputs user x and y mouse click and outputs the character found (if found)
+import type { Character } from "../types/types";
+const whoWasFound = (
+  xPos: number,
+  yPos: number,
+  selectedCharacter: string,
+  allCharacters: Character[]
+): string | null => {
+  const character = allCharacters.find(
+    (c) =>
+      c.xMin < xPos &&
+      c.xMax > xPos &&
+      c.yMin < yPos &&
+      c.yMax > yPos &&
+      selectedCharacter === c.name
+  );
 
-const whoWasFound = (xPos: number, yPos: number, character: string) => {
-  let characterFound = "";
-
-  const waldoXmin: number = 1819;
-
-  const waldoXmax: number = 1893;
-
-  const waldoYmin: number = 699;
-
-  const waldoYmax: number = 815;
-
-  if (
-    waldoXmin < xPos &&
-    waldoXmax > xPos &&
-    waldoYmin < yPos &&
-    waldoYmax > yPos &&
-    character == "Waldo"
-  ) {
-    characterFound = "Waldo";
-    return characterFound;
-  } else {
-    return false;
+  if (character) {
+    return character.name;
   }
+
+  return null; // no match found
 };
 
 export default whoWasFound;
