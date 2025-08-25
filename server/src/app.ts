7ed3d9afbcceee02fb69017823393b1fb1973e44
Francois <francois.thullier98@gmail.com>
2025-08-25 12:09:38 +0200

    Called cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,9 +3,12 @@ import express from "express";
 const app = express();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+
 import playerRoutes from "./routes/playerRoutes";
 import charactersRoutes from "./routes/charactersRoute";
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/api/players", playerRoutes);
 app.use("/api/characters", charactersRoutes);
 app.listen(PORT, () => {
